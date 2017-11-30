3ab32f5ae4c4d34188e6eb5f69f2e03c52faceb8
theantonioalmeida <tony-alm@hotmail.com>
2017-11-30 17:36:43 +0000

    Implement layout needed for sidebar

--- project1/list.php
+++ project1/list.php
@@ -21,79 +21,88 @@ $items = getListItems($listID);
 <script type="text/javascript" src="scripts/items.js" defer></script>
 
 <div class="container">
-  <?php if ($isLoggedIn) { ?>
-  <h4>
-    <?=$list['title']?>
-  </h4>
-  <h6>Created on <?=$list['creationDate']?></h6>
 
-  <?php } else { ?>
-  <!-- else here -->
-  <?php } ?>
-  <div class="items" >
-    <?php foreach($items as $item) { ?>
-    <div class="item" id="item<?=$item['id']?>">
-      <div class="item-left">
-        <input type="checkbox" id="<?=$item['id']?>" name="complete" 
-        <?php if($item['complete'] == 1) { ?> 
-        checked
-        <?php } ?>
-        >
-        <span><?=$item['description']?></span>
-        <span><?= date('d M', strtotime($item['dueDate']))?></span>
-      </div>
+  <div class="flex-container">
+    <div class="sidebar">
+      <p>Future Sidebar</p> 
+    </div>
 
-      <div class="item-edit hidden">
-        <form>
-          <div class="flex">
-            <input type="hidden" name="id" value="<?=$listID?>">
-            <div class="form-element">
-              <input type="text" name="editDescription" value="<?=$item['description']?>" required>
-              <span class="highlight"></span>
-              <span class="bar"></span>
-              <label>Description</label>
+    <div class="items">
+      <?php if ($isLoggedIn) { ?>
+      <h4>
+        <?=$list['title']?>
+      </h4>
+      <h6>Created on <?=$list['creationDate']?></h6>
+
+      <?php } else { ?>
+      <!-- else here -->
+      <?php } ?>
+      <?php foreach($items as $item) { ?>
+      <div class="item" id="item<?=$item['id']?>">
+        <div class="item-left">
+          <input type="checkbox" id="<?=$item['id']?>" name="complete" 
+          <?php if($item['complete'] == 1) { ?> 
+          checked
+          <?php } ?>
+          >
+          <span><?=$item['description']?></span>
+          <span><?= date('d M', strtotime($item['dueDate']))?></span>
+        </div>
+
+        <div class="item-edit hidden">
+          <form>
+            <div class="flex">
+              <input type="hidden" name="id" value="<?=$listID?>">
+              <div class="form-element">
+                <input type="text" name="editDescription" value="<?=$item['description']?>" required>
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Description</label>
+              </div>
+              <div>
+                <p>Due Date</p>
+                <input type="date" name="editDate" value="" required>
+              </div>
             </div>
             <div>
-              <p>Due Date</p>
-              <input type="date" name="editDate" value="" required>
+              <input class="button-primary" type="submit" value="Save">
+              <input class="button cancelEditItem" value="Cancel">
             </div>
-          </div>
-          <div>
-            <input class="button-primary" type="submit" value="Save">
-            <input class="button cancelEditItem" value="Cancel">
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
 
-      <div class="item-right">
-        <span><i id="edit<?=$item['id']?>" class="fa fa-pencil-square-o"></i></span>
-        <span><i id="delete<?=$item['id']?>" class="fa fa-trash"></i></span>
+        <div class="item-right">
+          <span><i id="assignUser<?=$item['id']?>" class="fa fa-user-plus"></i></span>
+          <span><i id="edit<?=$item['id']?>" class="fa fa-pencil-square-o"></i></span>
+          <span><i id="delete<?=$item['id']?>" class="fa fa-trash"></i></span>
+        </div>
+      </div>
+      <?php } ?>
+
+      <div class="add-item">
+        <a href="#" id="showAddItem"><i class="fa fa-plus"></i> Add a Task</a>
+        <div>
+          <form class="hidden" id="addItemForm">
+            <div>
+              <input type="hidden" name="id" value="<?=$listID?>">
+              <br>
+              <div class="form-element">
+                <input type="text" name="description" required>
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Description</label>
+              </div>
+            </div>
+            <div>
+              <input class="button-primary" type="submit" value="Add">
+              <input id="cancelAddItem" class="button" value="Cancel">
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-    <?php } ?>
-  </div>
 
-  <div class="add-item">
-    <a href="#" id="showAddItem"><i class="fa fa-plus"></i> Add a Task</a>
-    <div>
-      <form class="hidden" id="addItemForm">
-        <div>
-          <input type="hidden" name="id" value="<?=$listID?>">
-          <br>
-          <div class="form-element">
-            <input type="text" name="description" required>
-            <span class="highlight"></span>
-            <span class="bar"></span>
-            <label>Description</label>
-          </div>
-        </div>
-        <div>
-          <input class="button-primary" type="submit" value="Add">
-          <input id="cancelAddItem" class="button" value="Cancel">
-        </div>
-      </form>
-    </div>
-  </div>
+  </div> <!-- end .items -->
 
 </div>
 
