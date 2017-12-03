86c652f70fb237bb3b518f4b247b82024c21cce5
theantonioalmeida <tony-alm@hotmail.com>
2017-12-03 17:13:54 +0000

    Upload missing file from last commit

---
+++ project1/actions/action_assign_user.php
@@ -0,0 +1,22 @@
+<?php
+include_once(dirname(__DIR__) . '/includes/init.php');
+include_once(dirname(__DIR__) . '/database/user.php');
+include_once(dirname(__DIR__) . '/database/lists.php');
+
+if(isset($_SESSION['username'])) {
+	$username = $_SESSION['username'];
+
+	if ( !preg_match ("/^\d+$/", $_GET['itemID'])) {
+		die("ERROR: ID can only contain numbers");
+	}
+	if ( !preg_match ("/^[a-zA-Z][\w-]{1,18}(?![-_])\w$/", $_GET['assignedUser'])) {
+		die("ERROR: Username invalid");
+	}
+	
+	$itemID = $_GET["itemID"];
+	$assignedUser = $_GET["assignedUser"];
+
+	if (isItemAdmin($username, $itemID))
+		itemAssignUser($itemID, $assignedUser);
+}
+?>
