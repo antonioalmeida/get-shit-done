c217cad375fd7f6a6187338e597f330b0a8fbca0
cyrilico <jpdamas15@gmail.com>
2017-12-12 17:17:47 +0000

    Add item update extra verification

--- project1/actions/action_edit_priority.php
+++ project1/actions/action_edit_priority.php
@@ -3,12 +3,15 @@ include_once(dirname(__DIR__) . '/includes/init.php');
 include_once(dirname(__DIR__) . '/database/user.php');
 include_once(dirname(__DIR__) . '/database/lists.php');
 
-//TODO: add username and list validation
 if ( !preg_match ("/^\d+$/", $_GET['itemID'])) {
   die("ERROR: itemID can only contains numbers");
 }
 if ( !preg_match ("/^1|2|3$/", $_GET['priority'])) {
     die("ERROR: priority can only contain letters, numbers and most common punctuaction");
+}
+
+if(!(isset($_SESSION['username']) && isItemAdmin($_SESSION['username'], $_GET['itemID']))) {
+    die("ERROR: You can't perform this operation");
 }
 
 $itemID = $_GET["itemID"];
--- project1/actions/action_update_complete.php
+++ project1/actions/action_update_complete.php
@@ -3,12 +3,15 @@ include_once(dirname(__DIR__) . '/includes/init.php');
 include_once(dirname(__DIR__) . '/database/user.php');
 include_once(dirname(__DIR__) . '/database/lists.php');
 
-//TODO: add username and list validation
 if ( !preg_match ("/^\d+$/", $_GET['itemID'])) {
   die("ERROR: itemID can only contain numbers ");
 }
 if ( !preg_match ("/^0|1$/", $_GET['complete'])) {
   die("ERROR: Complete invalid ");
+}
+
+if(!(isset($_SESSION['username']) && isItemAdmin($_SESSION['username'], $_GET['itemID']))) {
+    die("ERROR: You can't perform this operation");
 }
 
 $itemID = $_GET["itemID"];
