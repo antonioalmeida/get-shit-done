34777913eed49d36fbbf26655753d60f7cae26ab
theantonioalmeida <tony-alm@hotmail.com>
2017-12-12 23:32:35 +0000

    Improve username autocomplete usability when adding members

--- project1/scripts/users.js
+++ project1/scripts/users.js
@@ -20,14 +20,22 @@ function showNoUsers() {
 function showSimilarUsersFinished() {
     let allNames = JSON.parse(this.responseText);
     showNoUsers();
-    searchResults.innerHTML += '<ul>';
-    for(let id in allNames)
-        searchResults.innerHTML += '<li class="no-bullets"><a href=/userprofile.php?username='+allNames[id].username+'>@'+allNames[id].username+'</a></li>';
-    searchResults.innerHTML += '</ul>';
+    for(let id in allNames) {
+        let result = document.createElement('p');
+        result.addEventListener('click', updateUsernameValue);
+        result.innerHTML = '@'+allNames[id].username;
+        searchResults.append(result);
+    }
 }
 
 function encodeForAjax(data) {
     return Object.keys(data).map(function(k){
         return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]);
     }).join('&');
+}
+
+function updateUsernameValue(event) {
+    let addMember = document.querySelector('input[name=addAdminUsername]');
+    let username = event.target.innerHTML.substr(1);
+    addMember.value = username;
 }
